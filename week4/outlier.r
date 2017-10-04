ec185cfa9697ee9feaf7ac08e9a15f6592663024
Cesar Marrades <cmarrades@hotmail.com>
2017-10-04 21:43:13 +0100

    outlier file

---
+++ week4/outlier.r
@@ -0,0 +1,29 @@
+athletes <- read.table('/home/epa/DAP/athletes.txt', header=", sep =',')
+
+#print summary stats of the athletes weight
+
+#obrienbeta@gmail.com
+
+#print (summary(athletes$Wt))
+
+#filter the data looking for w_polo
+
+wpolo <- athlete[athletes$Sport=="w_polo",]
+wpolo_men <- wpolo[wpolo$Sex==0,]
+
+wpolo <- athletes[athletes$Sport=="w_polo" & atj;etes&Sex=="0",]
+
+print (summary(wpolo))
+#subset(athletes, Sex==0 & Sport=="w_polo")
+
+#EXERCISE
+# Upload your outlier script calculating the correlation coefficient
+#for 
+#1.female basketball players WT & BMI
+
+#2. Male tennis players any 2 appropiate variables
+
+
+
+
+
