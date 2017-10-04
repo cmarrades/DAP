de1d2039b3efd96ad05ce52a795b4d668805e4bc
Cesar Marrades <cmarrades@hotmail.com>
2017-10-04 20:42:53 +0100

    first push

---
+++ week4/first.r
@@ -0,0 +1,18 @@
+#correlation coefficient analysis
+
+
+x <- c(2,3,4,1,9,7,6,5,4,9)
+
+# y is the dependant variable
+
+
+y <- c(5,6,7,1,2,3,8,9,8,2)
+
+res <- cor(x,y)
+
+print(res)
+
+
+#correlation coefficient calculation
+# is order dependent and moving the order around 
+#affects the coefficient calculation
