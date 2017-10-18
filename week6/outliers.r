e6b4df47a9b7faf955766b2c057b3d5b79b0610b
Cesar Marrades <cmarrades@hotmail.com>
2017-10-18 20:06:22 +0100

    outliers week6

--- week6/outliers.r
+++ week6/outliers.r
@@ -7,6 +7,8 @@ athletes$Sex <- factor(as.factor(athletes$Sex), label=c("M","F"))
 
 #discover outliers in the data in the height colum
 #use boxplot
+
+
 #filter the data and look for outliers for F basketball players
 #this means subset the data
 
@@ -19,3 +21,21 @@ with(subset(athletes, Sport=="b_ball" & Sex=="F"), boxplot(Ht))
 #equivalent
 #subdata <-subset(athletes, Sport=="b_ball", &	Sex=="F")
 #boxplot(subset)	
+
+
+
+doboxplots <- function (){
+ boxplot(athletes$Ht ~ athletes$Sex + athletes$Sport)
+# plotting height by pairs (gender and sport)
+}
+
+#pass in the M/F indicator and return the mean height for the Sex
+calcmeanHt <- function (gender) {
+	#calc the mean height for the subset where sex = MorF
+	subdata <- subset(athletes, Sex==gender)
+	mean <- mean(subdata$Ht)
+
+}
+
+doboxplots()
+print (calcmeanHt("M"))
